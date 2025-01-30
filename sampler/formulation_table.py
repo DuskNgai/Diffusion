19aa8bd4ea390a4b42ae8aba5f8d6d8b20eabe3e
DuskNgai <duskngai@gmail.com>
2025-01-30 09:06:26 +0800

    20250130. Unify langevin with diffusion.

--- sampler/formulation_table.py
+++ sampler/formulation_table.py
@@ -19,15 +19,16 @@ SAMPLER_FORMULATION_TABLE: dict[str, dict[str, FunctionType]] = {
         "scale_deriv_fn": lambda t: torch.exp(-t) if isinstance(t, torch.Tensor) else np.exp(-t),
         "sigma_fn": lambda t: torch.sqrt(1.0 - torch.exp(-2.0 * t)) if isinstance(t, torch.Tensor) else np.sqrt(1.0 - np.exp(-2.0 * t)),
         "sigma_deriv_fn": lambda t: torch.exp(-2.0 * t) / torch.sqrt(1.0 - torch.exp(-2.0 * t)) if isinstance(t, torch.Tensor) else np.exp(-2.0 * t) / np.sqrt(1.0 - np.exp(-2.0 * t)),
-        "nsr_inv_fn": lambda nsr: torch.log(1.0 + nsr ** 2) / 2.0 if isinstance(nsr, torch.Tensor) else np.log(1.0 + nsr ** 2) / 2.0,
+        "nsr_inv_fn": lambda nsr: torch.log1p(nsr ** 2) / 2.0 if isinstance(nsr, torch.Tensor) else np.log1p(nsr ** 2) / 2.0,
     },
     # ----- VE SDE -----
+    # NCSN: Noise Conditional Score Network, also for Langevin Dynamics
     "NCSN": {
-        "scale_fn": NotImplementedError,
-        "scale_deriv_fn": NotImplementedError,
-        "sigma_fn": NotImplementedError,
-        "sigma_deriv_fn": NotImplementedError,
-        "nsr_inv_fn": NotImplementedError,
+        "scale_fn": lambda t: torch.ones_like(t) if isinstance(t, torch.Tensor) else 1.0,
+        "scale_deriv_fn": lambda t: torch.zeros_like(t) if isinstance(t, torch.Tensor) else 0.0,
+        "sigma_fn": lambda t: torch.sqrt(t) if isinstance(t, torch.Tensor) else np.sqrt(t),
+        "sigma_deriv_fn": lambda t: 0.5 / torch.sqrt(t) if isinstance(t, torch.Tensor) else 0.5 / np.sqrt(t),
+        "nsr_inv_fn": lambda nsr: nsr ** 2,
     },
     "EDM": {
         "scale_fn": lambda t: torch.ones_like(t) if isinstance(t, torch.Tensor) else 1.0,
